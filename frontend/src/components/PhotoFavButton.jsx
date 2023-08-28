cc50b18de831dd1a26520868442c9f923cbe7f71
Jesse Sandhu <s.jesse23@gmail.com>
2023-08-27 22:54:55 -0700

    now uses selected variable that passes a prop.id if exists to toggle on click function and sends selected as prop to FavIcon

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -2,12 +2,19 @@ import React, { useCallback, useState } from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
+import PhotoDetailsModal from 'routes/PhotoDetailsModal';
+import PhotoListItem from './PhotoListItem';
 
-function PhotoFavButton() {
+function PhotoFavButton(props) {
+const selected = props.favourites.includes(props.id); // storing props of favourits - state / id from photolistitem to check if selected 
+console.log("props", props)
+const handleClick = function() {
+  props.toggleFav(props.id) //is this / onclick button where everything meets to pass this value to homeroute???? 
+}
   return (
-    <div className="photo-list__fav-icon">
+    <div className="photo-list__fav-icon" onClick={handleClick}> {/* add onclick to parent div and give it "prop value" of toggleFav function instead of call back */}
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={selected} /> {/* giving props of selected var to send for parameter("prop") to give desired effect*/}
       </div>
     </div>
   );
