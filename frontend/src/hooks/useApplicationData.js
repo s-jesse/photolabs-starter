2847464ef607aa58a7813849646f50ec982d2d43
Jesse Sandhu <s.jesse23@gmail.com>
2023-08-27 23:09:20 -0700

    useApplicationData component debugging to carry out all functionality of compenets requiring state

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,4 +1,4 @@
-import React, { useState, useReducer } from "react";
+import React, { useState } from "react";
 const useApplicationData = () => {
   const [showModal, setShowModal] = useState(false);
   const [selectedPhoto, setSelectedPhoto] = useState({});
@@ -10,37 +10,6 @@ const useApplicationData = () => {
 
   };
 
-  const intialState = {
-    displayedModal: false,
-    favorites: []
-  };
-  // commit original code before changes
-  // focus on initial state
-  // eg setting / updating favourites till works
-
-  export const ACTIONS = {
-    FAV_PHOTO_ADDED: 'FAV_PHOTO_ADDED',
-    FAV_PHOTO_REMOVED: 'FAV_PHOTO_REMOVED',
-    SET_PHOTO_DATA: 'SET_PHOTO_DATA',
-    SET_TOPIC_DATA: 'SET_TOPIC_DATA',
-    SELECT_PHOTO: 'SELECT_PHOTO',
-    DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS'
-  };
-
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case FAV_PHOTO_ADDED:
-        return { ...state, favourites: [...state.favourites, action.payload] };
-        { /* insert all relevant actions as case statements*/ }
-    }
-      default:
-throw new Error(
-  `Tried to reduce with unsupported action type: ${action.type}`
-);
-    }
-  }
-const useApplicationData = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
   const [favourites, setFavourites] = useState([]);
   const toggleFav = (photoid) => { // function for using state that is add to an array of favourites
     if (favourites.includes(photoid)) { // taking state and seeing if it includes photo - id
@@ -49,19 +18,18 @@ const useApplicationData = () => {
       return; // return to stop chain otherwise set code outside for favourties array to include photo
     }
     setFavourites((prev) => [...prev, photoid]); // setState to take prev data into spread array and add in props.id into array instead of pushing
-    // dispatch({type: ACTION.FAV_PHOTO_ADDED, payload: photoid})
+
   };
-};
-return {
-  showModal,
-  setShowModal,
-  selectedPhoto,
-  setSelectedPhoto,
-  handleClick,
-  toggleFav,
-  favourites,
-  setFavourites
-};
+  return {
+    showModal,
+    setShowModal,
+    selectedPhoto,
+    setSelectedPhoto,
+    handleClick,
+    toggleFav,
+    favourites,
+    setFavourites
+  };
 };
 
 export default useApplicationData;
