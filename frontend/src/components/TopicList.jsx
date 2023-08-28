e26b5e3a3c7bc6e494b853cc2162c72c1d5cf62c
Jesse Sandhu <s.jesse23@gmail.com>
2023-08-27 23:04:27 -0700

    modify topicList to use topics from mock folder and map through to send to TopicListItems as props for rendering

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import TopicListItem from "./TopicListItem";
 import "../styles/TopicList.scss";
 
 const sampleDataForTopicList = [
@@ -20,10 +20,21 @@ const sampleDataForTopicList = [
   },
 ];
 
-const TopicList = () => {
+const TopicList = (props) => {
+  // why no  braces / curly  brackets for topicLIst map
+  // should it hover?
   return (
     <div className="top-nav-bar__topic-list">
-      {/* Insert React */}
+    {/* {sampleDataForTopicList.map((topicList) => {
+    return(
+    <ul key={topicList.id}>
+        <h2>{topicList.slug} {topicList.title}</h2>
+    </ul>
+    )
+  }) } */}
+  {props.topics.map(topicListItems => 
+  <TopicListItem {...topicListItems} key={topicListItems.id} />
+  ) }
     </div>
   );
 };
--- frontend/src/components/TopicListItem.jsx
+++ frontend/src/components/TopicListItem.jsx
@@ -8,10 +8,10 @@ const sampleDataForTopicListItem = {
   label: "Nature",
 };
 
-const TopicListItem = () => {
+const TopicListItem = (props) => {
   return (
-    <div className="topic-list__item">
-      {/* Insert React */}
+    <div className="topic-list__item" >
+       <h2>{props.slug} {props.title}</h2>
     </div>
   );
 };
